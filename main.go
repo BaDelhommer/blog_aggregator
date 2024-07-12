5a38765d660af9e8c17147349949a4c8fe1ca6e5
BaDelhommer <badelhommer@gmail.com>
2024-07-12 13:41:43 -0500

    added create users

--- main.go
+++ main.go
@@ -1,12 +1,19 @@
 package main
 
 import (
+	"blog_aggregator/internal/database"
+	"database/sql"
 	"log"
 	"net/http"
 	"os"
 
 	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 )
+
+type apiConfig struct {
+	DB *database.Queries
+}
 
 func main() {
 	godotenv.Load(".env")
@@ -15,10 +22,29 @@ func main() {
 		log.Fatal("PORT .env not set")
 	}
 
+	dbUrl := os.Getenv("DATABASE_URL")
+	if dbUrl == "" {
+		log.Fatal("DATABSE_URL .env not set")
+	}
+
+	db, err := sql.Open("postgres", dbUrl)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	dbQueries := database.New(db)
+
+	apiCfg := apiConfig{
+		DB: dbQueries,
+	}
+
 	mux := http.NewServeMux()
 
 	mux.HandleFunc("GET /v1/healthz", handlerReadiness)
 	mux.HandleFunc("GET /v1/err", handleErr)
+
+	mux.HandleFunc("POST /v1/users", apiCfg.handlerUsersCreate)
+
 	srv := &http.Server{
 		Addr:    ":" + port,
 		Handler: mux,
