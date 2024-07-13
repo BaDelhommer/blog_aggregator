5629d6ded7a54156b664f71bac655d0a7a9bfc68
BaDelhommer <badelhommer@gmail.com>
2024-07-12 19:10:56 -0500

    added get all feeds

--- handler_feed.go
+++ handler_feed.go
@@ -39,3 +39,12 @@ func (cfg *apiConfig) handlerFeedCreate(w http.ResponseWriter, r *http.Request, 
 
 	respondWithJSON(w, http.StatusOK, databaseFeedToFeed(feed))
 }
+
+func (cfg *apiConfig) handlerFeedsGet(w http.ResponseWriter, r *http.Request) {
+	feeds, err := cfg.DB.GetFeeds(r.Context())
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Couldn't load feeds")
+	}
+
+	respondWithJSON(w, http.StatusOK, databaseFeedsToFeeds(feeds))
+}
